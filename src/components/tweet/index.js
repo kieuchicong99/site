04c1ce9610947e3ff18d62dabafa22a845629b8f
cong.kieu <cong.kieu@mechmaster.io>
2022-08-11 17:06:39 +0700

    blog 2202-08-11

---
+++ src/components/tweet/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {TwitterTweetEmbed} from 'react-twitter-embed';
+
+export default function Tweet({id}) {
+  return (
+    <TwitterTweetEmbed
+    tweetId={id}
+  />
+  
+  );
+}
